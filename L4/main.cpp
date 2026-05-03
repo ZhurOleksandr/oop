6e05c59ab3f3ed4c1b936737aaddf19186c2b159
ZhurOleksandr <volodymyr.bondo.o@gmail.com>
2026-05-03 19:18:52 +0300

    Add files via upload

---
+++ L4/main.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <Windows.h>
+
+using namespace std;
+
+template <class T>
+void procVec(vector<T>& v)
+{
+	for (int i = 0; i < v.size(); i++)
+		cin >> v[i];
+
+	sort(v.begin(), v.end());
+
+	cout << "Відсортовані вектори:\n";
+	for (int i = 0; i < v.size(); i++)
+		cout << v[i] << " ";
+	cout << endl;
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	int n;
+
+	cout << "Введіть розмір: ";
+	cin >> n;
+
+	vector<int> v(n);
+
+	cout << "Введіть елементи:\n";
+	procVec(v);
+
+	return 0;
+}
